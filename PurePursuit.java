2eb322fc7abf3ea0dac968d4fb7d84986e055df8
webbgamers <webbgamers@gmail.com>
2019-09-29 21:18:01 -0400

    pure pursuit
    Trying to figure some pure pursuit stuffout from a tutorial. Not going so well. Please don't mess with anything in here.

---
+++ PurePursuit.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Pure Pursuit Test", group="Linear Opmode")
+public class PurePursuit extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+        }
+    }
+}
---
+++ PurePursuitMovement.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PurePursuitMovement {
+    public static void goToPosition(double x, double y, double movementSpeed) {
+        double absoluteAngleToTarget = Math.atan2(y-worldYPosition)
+    }
+}
