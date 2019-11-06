133181e73ae152f6b42b943f52cc073bedf5f2c5
Rishabh Kanodia <rishabhkanodia6@gmail.com>
2019-11-05 19:50:10 -0500

    PeLeAsE hElP

---
+++ AutoParkWall.java
@@ -0,0 +1,47 @@
+// simple autonomous program that drives bot forward 2 seconds then ends.
+
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@Autonomous(name="Drive Forward", group="Auto")
+public class AutoParkWall extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        DcMotor leftDrive = hardwareMap.dcMotor.get("left_drive");
+        DcMotor rightDrive = hardwareMap.dcMotor.get("right_drive");
+        CRServo claw = hardwareMap.crservo.get("claw");
+        DcMotor leftArmPivot = hardwareMap.dcMotor.get("arm_pivot_left");
+        DcMotor rightArmPivot = hardwareMap.dcMotor.get("arm_pivot_right");
+
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Mode", "waiting");
+        telemetry.update();
+
+        init();
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        //rest unused motors
+        claw.setPower(0.0);
+        leftArmPivot.setPower(0.0);
+        rightArmPivot.setPower((0.0));
+
+        //Go forward to park on tape
+        leftDrive.setPower(0.25);
+        rightDrive.setPower(0.25);
+
+        sleep(1000);
+
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+    }
+}
