dc4205e9983eeab2b4e21ca7e795c05d9b02b5a7
Trisha <trisha.sakamuri@gmail.com>
2019-11-07 20:00:14 -0500

    Update AutoParkWall.java

--- AutoParkWall.java
+++ AutoParkWall.java
@@ -3,6 +3,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+<<<<<<< HEAD
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -30,19 +31,40 @@ public class AutoParkWall extends LinearOpMode
         rightDrive = hardwareMap.dcMotor.get("right_drive");
         claw = hardwareMap.servo.get("claw");
         armPivot = hardwareMap.dcMotor.get("arm_pivot");
+=======
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+
+@Autonomous(name="Drive Forward", group="Auto")
+public class AutoParkWall extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        DcMotor leftDrive = hardwareMap.dcMotor.get("left_drive");
+        DcMotor rightDrive = hardwareMap.dcMotor.get("right_drive");
+        CRServo claw = hardwareMap.crservo.get("claw");
+        DcMotor leftArmPivot = hardwareMap.dcMotor.get("arm_pivot_left");
+        DcMotor rightArmPivot = hardwareMap.dcMotor.get("arm_pivot_right");
+>>>>>>> d28cefb4818d447b417a831bf0786a9af3ec2775
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
+<<<<<<< HEAD
         // wait for start button.
+=======
+>>>>>>> d28cefb4818d447b417a831bf0786a9af3ec2775
         init();
         waitForStart();
 
         telemetry.addData("Mode", "running");
         telemetry.update();
 
+<<<<<<< HEAD
         // set both motors to 25% power.
 
         leftDrive.setPower(0.25);
@@ -51,6 +73,18 @@ public class AutoParkWall extends LinearOpMode
         armPivot.setPower(0.0);
 
         sleep(2000);        // wait for 2 seconds.
+=======
+        //rest unused motors
+        claw.setPower(0.0);
+        leftArmPivot.setPower(0.0);
+        rightArmPivot.setPower((0.0));
+
+        //Go forward to park on tape
+        leftDrive.setPower(0.25);
+        rightDrive.setPower(0.25);
+
+        sleep(1000);
+>>>>>>> d28cefb4818d447b417a831bf0786a9af3ec2775
 
         leftDrive.setPower(0.0);
         rightDrive.setPower(0.0);
