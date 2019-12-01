2ca9bc529ec4672b51047d5c5462998109dac2e0
Rishabh Kanodia <rishabhkanodia6@gmail.com>
2019-12-01 15:44:37 -0500

    Update StaticArmOpMode.java

--- StaticArmOpMode.java
+++ StaticArmOpMode.java
@@ -16,9 +16,13 @@ public class StaticArmOpMode extends LinearOpMode {
         DcMotor leftArmPivot = hardwareMap.dcMotor.get("arm_pivot_left");
         DcMotor rightArmPivot = hardwareMap.dcMotor.get("arm_pivot_right");
         Servo claw = hardwareMap.servo.get("claw");
+        Servo rightFang = hardwareMap.servo.get("rightServo");
+        Servo leftFang = hardwareMap.servo.get("leftServo");
+
         boolean was_slowmode_pressed = false;
         boolean claw_open = false;
         boolean slow_mode = false;
+        boolean fang_open = false;
         double PIVOT_POWER = 0.2;
 
         telemetry.addData("Status", "Initialized");
@@ -32,9 +36,9 @@ public class StaticArmOpMode extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            double leftPower;
-            double rightPower;
-            double armPivotPower;
+            double leftPower = 0;
+            double rightPower = 0;
+            double armPivotPower = 0.5;
 
             // toggle slow mode when A is pressed
             if (gamepad1.a) {
@@ -42,23 +46,32 @@ public class StaticArmOpMode extends LinearOpMode {
                     slow_mode = !slow_mode;
                 }
                 was_slowmode_pressed = true;
-            }
-            else {
+            } else {
                 was_slowmode_pressed = false;
             }
 
-            // toggle claw position when A/Y is pressed
+            // toggle claw position when A/Y is pressed on gamepad2
             if ((gamepad2.a) && (claw_open)) {
-                    claw_open = false;
-                    claw.setPosition(100);
+                claw_open = false;
+                claw.setPosition(100);
+            } else if ((gamepad2.y) && (!claw_open)) {
+                claw_open = true;
+                claw.setPosition(0);
             }
-            else if ((gamepad2.y) && (!claw_open)) {
-                    claw_open = true;
-                    claw.setPosition(0);
+
+            //Toggle Fang Position when A/Y is pressed on gamepad1
+            if ((gamepad1.a) && (fang_open)) {
+                fang_open = false;
+                rightFang.setPosition(90);
+                leftFang.setPosition(90);
+            } else if ((gamepad1.a) && (!fang_open)) {
+                fang_open = true;
+                rightFang.setPosition(0);
+                leftFang.setPosition(0);
             }
 
             // Decrease drivetrain speed if slowmode is on
-            if (slow_mode) {
+        if (slow_mode) {
                 leftPower = 0.5 * gamepad1.left_stick_y;
                 rightPower = 0.5 * gamepad1.right_stick_y;
             }
