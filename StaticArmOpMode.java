503704df650141066c86e2c6d34b8a0b4f89fe7c
Rishabh Kanodia <rishabhkanodia6@gmail.com>
2020-01-15 19:37:49 -0500

    Update StaticArmOpMode.java

--- StaticArmOpMode.java
+++ StaticArmOpMode.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -7,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+@Disabled
 @TeleOp(name="OpMode Static Arm", group="Linear Opmode")
 
 public class StaticArmOpMode extends LinearOpMode {
