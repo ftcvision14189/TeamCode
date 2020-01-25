70ba3601babbf4adb3ca5d635fd3f3ef83204f95
Rishabh Kanodia <rishabhkanodia6@gmail.com>
2020-01-24 19:59:15 -0500

    Update MecanumFangBlue.java

--- MecanumFangBlue.java
+++ MecanumFangBlue.java
@@ -171,7 +171,7 @@ import com.qualcomm.robotcore.hardware.Servo;
             rightFrontMotor.setPower(-0.6);
             leftRearMotor.setPower(-0.6);
             rightRearMotor.setPower(-0.6);
-            sleep(1100);
+            sleep(1075);
 
             leftFrontMotor.setPower(0.0);
             rightFrontMotor.setPower(0.0);
