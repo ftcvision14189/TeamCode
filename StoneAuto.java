3c011a3fa0b53305fc97d315a042834a0c702d71
Trisha <trisha.sakamuri@gmail.com>
2020-02-22 11:48:51 -0500

    Update StoneAuto.java

--- StoneAuto.java
+++ StoneAuto.java
@@ -1,15 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 public class StoneAuto {
 
     @Autonomous(name = "MecanumWallPark", group = "Auto")
+   // @Disabled
     public class MecanumWallPark extends LinearOpMode {
 
         DcMotor leftFrontMotor = null;
@@ -32,12 +38,16 @@ public class StoneAuto {
             rightRearMotor = hardwareMap.dcMotor.get("rightRear");
             leftFang = hardwareMap.servo.get("leftServo");
             rightFang = hardwareMap.servo.get("rightServo");
+            intakeRight = hardwareMap.dcMotor.get("intakeRight");
+            intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
 
 
             leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
             rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
             leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
             rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
+            intakeRight.setDirection(DcMotor.Direction.REVERSE);
+            intakeRight.setDirection(DcMotor.Direction.FORWARD);
 
             telemetry.addData("Mode", "waiting");
             telemetry.update();
@@ -50,17 +60,95 @@ public class StoneAuto {
             telemetry.update();
 
             // set both motors to 25% power.
+            strafe(5,0.25);
+            strafe(-5,0.25);
+            moveStraight(5, 0.25);
+            moveStraight(-5, 0.25);
+            rotate(5,0.25);
+            rotate(-5,0.25);
 
-            leftFrontMotor.setPower(0.6);
-            rightFrontMotor.setPower(0.6);
-            leftRearMotor.setPower(0.6);
-            rightRearMotor.setPower(0.6);
-            sleep(2000);
-
-            leftFrontMotor.setPower(0.0);
-            rightFrontMotor.setPower(0.0);
-            leftRearMotor.setPower(0.0);
-            rightRearMotor.setPower(0.0);
         }
+
+        // Define movement functions
+        void strafe(float rotations, double power) {
+            leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftFrontMotor.setTargetPosition(Math.round(1120 * rotations));
+            rightFrontMotor.setTargetPosition(Math.round(-1120 * rotations));
+            leftRearMotor.setTargetPosition(Math.round(-1120 * rotations));
+            rightRearMotor.setTargetPosition(Math.round(1120 * rotations));
+
+            leftFrontMotor.setPower(power);
+            rightFrontMotor.setPower(power);
+            leftRearMotor.setPower(power);
+            rightRearMotor.setPower(power);
+
+            leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            while (leftFrontMotor.isBusy() && rightFrontMotor.isBusy() && leftRearMotor.isBusy() && rightRearMotor.isBusy() && opModeIsActive());
+
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftRearMotor.setPower(0);
+            rightRearMotor.setPower(0);
+        }
+
+        void moveStraight(float rotations, double power) {
+            leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftFrontMotor.setTargetPosition(Math.round(1120 * rotations));
+            rightFrontMotor.setTargetPosition(Math.round(1120 * rotations));
+            leftRearMotor.setTargetPosition(Math.round(1120 * rotations));
+            rightRearMotor.setTargetPosition(Math.round(1120 * rotations));
+
+            leftFrontMotor.setPower(power);
+            rightFrontMotor.setPower(power);
+            leftRearMotor.setPower(power);
+            rightRearMotor.setPower(power);
+
+            leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            while (leftFrontMotor.isBusy() && rightFrontMotor.isBusy() && leftRearMotor.isBusy() && rightRearMotor.isBusy() && opModeIsActive());
+
+            leftFrontMotor.setPower(0);
+            rightFrontMotor.setPower(0);
+            leftRearMotor.setPower(0);
+            rightRearMotor.setPower(0);
+        }
+
+        private void rotate(float rotations, double power) {
+            leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightRearMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            leftFrontMotor.setTargetPosition(Math.round(-1120 * rotations));
+            rightFrontMotor.setTargetPosition(Math.round(1120 * rotations));
+            leftRearMotor.setTargetPosition(Math.round(-1120 * rotations));
+            rightRearMotor.setTargetPosition(Math.round(1120 * rotations));
+
+            leftFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFrontMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightRearMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            leftFrontMotor.setPower(power);
+            rightFrontMotor.setPower(power);
+            leftRearMotor.setPower(power);
+            rightRearMotor.setPower(power);
+        }
+
     }
 }
