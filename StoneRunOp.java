50b98f8b059bc1fb1544e57f89167539be086d89
webbgamers <webbgamers@gmail.com>
2020-02-23 14:08:06 -0500

    more unbaddening of teleop

--- StoneRunOp.java
+++ StoneRunOp.java
@@ -77,14 +77,8 @@ public class StoneRunOp extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
 
-            if (gamepad1.y && !wasYPressed){
-                slowmode = !slowmode;
-                wasYPressed = true;
-            }
-
-            else if (!gamepad1.y) {
-                wasYPressed = false;
-            }
+            slowmode = (gamepad1.y && !wasYPressed);
+            wasYPressed = gamepad1.y;
 
             if (slowmode) {
                 joyScale = slowModeSpeed;
