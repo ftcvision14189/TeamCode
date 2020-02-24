4f8f1ffd984d3871854259a9eb63eaf9355a0c2f
Rishabh Kanodia <rishabhkanodia6@gmail.com>
2020-02-23 21:59:32 -0500

    Update StoneRunOP.java

--- StoneRunOP.java
+++ StoneRunOP.java
@@ -9,9 +9,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 
 
-@TeleOp(name="StoneRunOP", group="Linear Opmode") // @Autonomous(...) is the other common choice
+@TeleOp(name="StoneRunOP", group="Linear Opmode")
 
-public class StoneRunOp extends LinearOpMode {
+public class StoneRunOP extends LinearOpMode {
 
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
@@ -46,15 +46,11 @@ public class StoneRunOp extends LinearOpMode {
 
     // operational constants
     double joyScale = 0.8;
-    double motorMax = 1; // Limit motor power to this value for Andymark RUN_USING_ENCODER mode
+    double motorMax = 1;
 
     @Override
     public void runOpMode() {
 
-        /* Initialize the hardware variables. Note that the strings used here as parameters
-         * to 'get' must correspond to the names assigned during the robot configuration
-         * step (using the FTC Robot Controller app on the phone).
-         */
         leftFrontMotor = hardwareMap.dcMotor.get("leftFront");
         rightFrontMotor = hardwareMap.dcMotor.get("rightFront");
         leftRearMotor = hardwareMap.dcMotor.get("leftRear");
@@ -67,25 +63,18 @@ public class StoneRunOp extends LinearOpMode {
         tapePark = hardwareMap.servo.get("tapePark");
 
 
-        // Set the drive motor direction:
-        // "Reverse" the motor that runs backwards when connected directly to the battery
-        // These polarities are for the Neverest 20 motors
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
         rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
 
 
-        // Set the drive motor run modes:
-        // "RUN_USING_ENCODER" causes the motor to try to run at the specified fraction of full velocity
-        // Note: We were not able to make this run mode work until we switched Channel A and B encoder wiring into
-        // the motor controllers. (Neverest Channel A connects to MR Channel B input, and vice versa.)
         leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftRearMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightRearMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // Wait for the game to start (driver presses PLAY)
+
         telemetry.addData("Status", "Waiting for start.");
         telemetry.update();
         waitForStart();
@@ -96,7 +85,6 @@ public class StoneRunOp extends LinearOpMode {
         boolean capstone_in = true;
 
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
 
